5540fc0e38ca5b76487650c3f520465773109b97
Emerson Pinheiro <emersonline2007@gmail.com>
2019-04-29 22:21:24 -0400

    #25 - desenvolvendo PHP - o Retorno - Curso Loja Virtual - PHP #26 - continua na #27 #27 - Url Amigável( SEO ) sem o método $_GET - Curso Loja Virtual - PHP #28 - Url Amigável( SE0 ) sem o método $_GET - Curso Loja Virtual - PHP #29 - Url Amigável( SE0 ) sem o método $_GET - Curso Loja Virtual - PHP

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php 
+	//var_dump($_SERVER['REDIRECT_URL']);die;
 
-echo "teste";
+	require_once"header.php";
+
+	if(isset($_SERVER['REDIRECT_URL']))
+	{
+		$r_URL = substr($_SERVER['REDIRECT_URL'], 1);
+		$r_URL = explode('/', $r_URL);
+
+		//primeiro array passado
+		if(file_exists($r_URL[0].'.php')) 
+		{
+			require_once $r_URL[0].'.php';
+			
+			if(strrchr($r_URL[0], '.'))
+			{
+				$id = explode('.', $r_URL[0]);
+				$id = intval($id[1]);
+			}
+		}
+		else{require_once "404.php";}
+	}
+	else
+	{
+		require_once"home.php";
+	}
+
+	require_once"footer.php";
